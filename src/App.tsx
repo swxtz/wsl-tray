a390472200dc3ae470574ad4a82702cfc0ad443a
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2025-03-13 04:47:45 -0300

    feat: add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./main.css"
+
+export function App() {
+  return (
+    <div className="bg-zinc-900 h-screen w-screen text-white font-display mx-auto">
+      <h1 className="mx-auto w-fit pt-12 text-3xl">WSL Tray</h1>
+    </div>
+  );
+}
